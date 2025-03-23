1e84849210d10dc577becfae5660f76f5d8c01b4
Yuvraj Singh Kumpavat <115008153+Euphorus@users.noreply.github.com>
2025-03-23 17:38:19 +0000

    0.5%

---
+++ Source/Racing_Physics/Public/GM_Racist.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Racist.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACING_PHYSICS_API AGM_Racist : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
